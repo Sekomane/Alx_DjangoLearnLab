4f6c85de8717cd115198ba0e2ee7c7dff50307bc
Rorisang Sekomane <sekomanerorisang904@gmail.com>
2026-01-22 10:56:14 +0200

    Fix Library import for class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 
-# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(
-        request,
-        "relationship_app/list_books.html",
-        {"books": books}
-    )
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view: display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
