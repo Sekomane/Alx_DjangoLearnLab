eab320c0fd7517382832ba6c90590c33e2b41f13
Rorisang Sekomane <sekomanerorisang904@gmail.com>
2026-01-22 12:09:36 +0200

    Fix signals import path in AppConfig

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'relationship_app',
+    'relationship_app.apps.RelationshipAppConfig',
 ]
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Book
 from .models import Library
-
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 def list_books(request):
     books = Book.objects.all()
@@ -26,5 +27,35 @@ def register(request):
             return redirect("list_books")
     else:
         form = UserCreationForm()
+
+
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
     return render(request, "relationship_app/register.html", {"form": form})
