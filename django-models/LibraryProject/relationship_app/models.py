9568faca769daf4a2663d1d0f4da0c27c9fe4104
Rorisang Sekomane <sekomanerorisang904@gmail.com>
2026-02-01 21:59:30 +0200

    Add custom permissions Meta class to Book model in relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,8 +13,17 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
     def __str__(self):
         return self.title
+
+
 
 
 class Library(models.Model):
@@ -39,8 +48,6 @@ class UserProfile(models.Model):
         return f"{self.user.username} - {self.role}"
 
 
-
-# 📚 Librarian domain model (NOT auth-related)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(
