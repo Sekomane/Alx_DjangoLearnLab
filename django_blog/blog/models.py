065ea2d85194fc914328f15946494be55a32c3a8
Rorisang Sekomane <sekomanerorisang904@gmail.com>
2026-02-15 16:02:15 +0200

    Reset migrations and fixed django-taggit integration

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
 
     title = models.CharField(max_length=200)
-
     content = models.TextField()
-
     published_date = models.DateTimeField(auto_now_add=True)
 
     author = models.ForeignKey(
@@ -16,7 +15,7 @@ class Post(models.Model):
         related_name="posts"
     )
 
-    tags = models.ManyToManyField("Tag", related_name="posts", blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
@@ -31,15 +30,11 @@ class Comment(models.Model):
     )
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
-
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
 
 
 class Tag(models.Model):
