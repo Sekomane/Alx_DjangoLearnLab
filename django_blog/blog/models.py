bc9d7b06a0c272afef058bbab9124c6c3344afad
Rorisang Sekomane <sekomanerorisang904@gmail.com>
2026-02-15 15:46:09 +0200

    Fixed Comment model order in models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 
+# FIRST: Post model
 class Post(models.Model):
 
     title = models.CharField(max_length=200)
@@ -20,5 +20,26 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse("post-detail", kwargs={"pk": self.pk})
+
+# SECOND: Comment model
+class Comment(models.Model):
+
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
